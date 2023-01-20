280e979dccd815699dd5cf165ae5bff9f34d20f6
fatemehkhanali <fatemehkhanali1775@gmail.com>
2023-01-20 18:16:54 +0330

    create and design song page and create adaptor Song for show data in list veiw

--- app/src/main/java/com/example/khanaliazizi_melobit/AdaptorSearchSong.java
+++ app/src/main/java/com/example/khanaliazizi_melobit/AdaptorSearchSong.java
@@ -12,7 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.arghavanazizimusicplayer.model.SearchModel;
+import com.example.khanaliazizi_melobit.model.SearchModel;
 
 import java.util.List;
 
